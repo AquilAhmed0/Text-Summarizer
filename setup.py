0ee231eae126be4a14bca621411ee4d75cf733c5
AquilAhmed0 <aquilahmed425@gmail.com>
2023-08-10 16:33:22 +0530

    data ingestion notebook added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
     __version__ = "0.0.0"
 
-    REPO_NAME = "Tect-Summarizer"
+    REPO_NAME = "Text-Summarizer"
     AUTHOR_USER_NAME = "AquilAhmed0" 
     SRC_REPO = "textSummarizer"
     AUTHOR_EMAIL = "aquilahmed425@gmail.com"
